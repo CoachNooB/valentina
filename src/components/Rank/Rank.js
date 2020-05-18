84a64119b7e527973b10834629699333cc5250a1
CoachNooB <noobfromid@gmail.com>
2020-05-18 22:47:39 +0700

    Front End

---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,17 @@
+import React from 'react';
+// import './ImageLinkForm.css';
+
+const Rank = () => {
+    return (
+        <div>
+           <div className='f3'>
+                {'Hello, Your Current Rank is ...'}
+           </div>
+           <div className='f1'>
+                {'#1'}
+           </div>
+        </div>
+    );
+}
+
+export default Rank;
