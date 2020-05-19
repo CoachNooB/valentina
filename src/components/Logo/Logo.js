03e6875b5ad953440fda88701218416136249a22
CoachNooB <noobfromid@gmail.com>
2020-05-19 13:09:58 +0700

    Add Face Detect Funtion

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -6,8 +6,8 @@ import './Logo.css';
 const Logo = () => {
     return (
         <div className='ma4 mt0'>
-            <Tilt className="Tilt" options={{ max: 25 }} style={{ height: 150, width: 150 }}>
-                <div className="Tilt-inner"><img style={{paddingTop: '5px'}} src={brain} alt='brain-logo' /></div>
+            <Tilt className='Tilt' options={{ max: 25 }} style={{ height: 150, width: 150 }}>
+                <div className='Tilt-inner'><img style={{paddingTop: '5px'}} src={brain} alt='brain-logo' /></div>
             </Tilt>
         </div>
     )
