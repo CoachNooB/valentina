5b4cfc1b39d3c70eea9419f7b0befac87689c59d
Antonius Harry <55129369+CoachNooB@users.noreply.github.com>
2020-06-10 22:38:52 +0700

    Connect Front and Back

--- src/App.js
+++ src/App.js
@@ -36,8 +36,25 @@ class App extends Component {
       imageUrl: '',
       box: {},
       route: 'signin',
-      isSignedIn: false
+      isSignedIn: false,
+      user: {
+        id: '',
+        name: '',
+        email: '',
+        entries: 0,
+        joined: ''
+      }
     }
+  }
+
+  loadUser = (data) => {
+    this.setState({user: {
+        id: data.id,
+        name: data.name,
+        email: data.email,
+        entries: data.entries,
+        joined: data.joined
+    }})
   }
 
   calculateFaceLocation = (data) => {
@@ -65,8 +82,23 @@ class App extends Component {
   onButtonSubmit = () => {
     this.setState({ imageUrl: this.state.input })
     app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
-    .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
-    .catch(err => console.log(err))
+    .then(response => {
+      if(response) {
+        fetch('http://localhost:3001/image', {
+          method: 'put',
+          headers: {'Content-Type': 'application/json'},
+          body: JSON.stringify({
+            id: this.state.user.id
+          })
+        })
+          .then(response => response.json())
+          .then(count => {
+            this.setState(Object.assign(this.state.user, {entries: count}))
+          })
+      }
+      this.displayFaceBox(this.calculateFaceLocation(response))
+    })
+    .catch(err => console.log(err));
   }
 
   onRouteChange = (route) => {
@@ -79,7 +111,7 @@ class App extends Component {
   }
 
   render() {
-     const { isSignedIn, imageUrl, route, box } = this.state;
+    const { isSignedIn, imageUrl, route, box } = this.state;
     return (
       <div className="App">
         <Particles className='particles'
@@ -89,19 +121,21 @@ class App extends Component {
         { this.state.route === 'home'
         ? <div>
             <Logo />
-            <Rank />
+            <Rank name={ this.state.user.name } entries={ this.state.user.entries } />
             <ImageLinkForm onInputChange={ this.onInputChange } onButtonSubmit={ this.onButtonSubmit } />
             <FaceRecognition box={ box } imageUrl={ imageUrl } />
           </div>
         : (
           route === 'signin'
-          ?<Signin onRouteChange={ this.onRouteChange } />
-          :<Register onRouteChange={ this.onRouteChange } />
+          ?<Signin loadUser={ this.loadUser } onRouteChange={ this.onRouteChange } />
+          :<Register loadUser={ this.loadUser } onRouteChange={ this.onRouteChange } />
         )
         }
       </div>
     );
   }
 }
+
+
 
 export default App;
